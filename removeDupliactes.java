2873f0e2fa8aebcafd9de03cce9b3af635063edd
sskyhigh <windowshao_123@yahoo.com>
2023-05-23 21:03:53 -0400

    Update

--- removeDupliactes.java
+++ removeDupliactes.java
@@ -1,15 +1,15 @@
 public class removeDupliactes {
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
     }
-    public int removeDuplicates(int [] nums){
-        int count = 0;
-        for(int i = 0; i<nums.length; ++i){
 
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int num : nums) {
+            if (i < 1 || num > nums[i - 1]) {
+                nums[i++] == num;
+            }
         }
-        for(int i = 0; i<nums.length; ++i){
-            
-        }
-        return count;
+        return i;
     }
 }

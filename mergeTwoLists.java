f4b1a1d873903bd714d23045e2e389913adfc8cf
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2023-10-06 17:47:03 -0400

    finished mergeTwoList leetcode

--- mergeTwoLists.java
+++ mergeTwoLists.java
@@ -1,5 +1,71 @@
 public class mergeTwoLists {
     public static void main(String[] args) {
-        
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+
+        ListNode node4 = new ListNode(1);
+        ListNode node5 = new ListNode(3);
+        ListNode node6 = new ListNode(4);
+
+        node4.next = node5;
+        node5.next = node6;
+
+        mergeTwoLists merge = new mergeTwoLists();
+        ListNode mergedList = merge.mergeTwoLists(node1, node4);
+
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode head, temp;
+        if (list1.val < list2.val) {
+            temp = head = new ListNode(list1.val);
+            list1 = list1.next;
+        } else {
+            temp = head = new ListNode(list2.val);
+            list2 = list2.next;
+        }
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                temp.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                temp.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+        while (list1 != null) {
+            temp.next = new ListNode(list1.val);
+            list1 = list1.next;
+            temp = temp.next;
+        }
+        while (list2 != null) {
+            temp.next = new ListNode(list2.val);
+            list2 = list2.next;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
     }
 }
